7e8b06c0e0237ffcd730247b8d966140294a5391
Jonathan B <31107158+jballo@users.noreply.github.com>
2024-12-05 15:31:19 -0800

    Noted where the form will be in code.

--- app/page.js
+++ app/page.js
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div>Text</div>
+      <div id="header">
+        <h1>Investor Agent 📈</h1>
+      </div>
+      <div id="body">{/* Form Placement */}</div>
     </>
   );
 }
